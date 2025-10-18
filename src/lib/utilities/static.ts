8dd2afe22bec6ac1c2b5bd477e7846c33eeb1799
nailuslabs <nailuslab@gmail.com>
2025-10-18 22:53:32 +0200

    mise a jours static des classes align-self

--- src/lib/utilities/static.ts
+++ src/lib/utilities/static.ts
@@ -346,5 +346,152 @@ export const staticUtilities: StaticUtility = {
     },
   },
 
+  // alignSelf
+  // Utilities for controlling ....
+  'self-auto': {
+    'utility': {
+      'align-self': 'auto',
+    },
+    'meta': {
+      'group': 'alignSelf',
+      'order': 1,
+    },
+  },
+  'self-normal': {
+    'utility': {
+      'align-self': 'normal',
+    },
+    'meta': {
+      'group': 'alignSelf',
+      'order': 2,
+    },
+  },
+  'self-center': {
+    'utility': {
+      'align-self': 'center',
+    },
+    'meta': {
+      'group': 'alignSelf',
+      'order': 3,
+    },
+  },
+  'self-start': {
+    'utility': {
+      'align-self': 'start',
+    },
+    'meta': {
+      'group': 'alignSelf',
+      'order': 4,
+    },
+  },
+  'self-end': {
+    'utility': {
+      'align-self': 'end',
+    },
+    'meta': {
+      'group': 'alignSelf',
+      'order': 5,
+    },
+  },
+  'self-flex-start': {
+    'utility': {
+      'align-self': 'flex-start',
+    },
+    'meta': {
+      'group': 'alignSelf',
+      'order': 6,
+    },
+  },
+  'self-flex-end': {
+    'utility': {
+      'align-self': 'flex-end',
+    },
+    'meta': {
+      'group': 'alignSelf',
+      'order': 7,
+    },
+  },
+  'self-baseline': {
+    'utility': {
+      'align-self': 'baseline',
+    },
+    'meta': {
+      'group': 'alignSelf',
+      'order': 8,
+    },
+  },
+  'self-first-baseline': {
+    'utility': {
+      'align-self': 'first baseline',
+    },
+    'meta': {
+      'group': 'alignSelf',
+      'order': 9,
+    },
+  },
+  'self-last-baseline': {
+    'utility': {
+      'align-self': 'last baseline',
+    },
+    'meta': {
+      'group': 'alignSelf',
+      'order': 10,
+    },
+  },
+  'self-stretch': {
+    'utility': {
+      'align-self': 'stretch',
+    },
+    'meta': {
+      'group': 'alignSelf',
+      'order': 11,
+    },
+  },
+  'self-safe-center': {
+    'utility': {
+      'align-self': 'safe center',
+    },
+    'meta': {
+      'group': 'alignSelf',
+      'order': 12,
+    },
+  },
+  'self-unsafe-center': {
+    'utility': {
+      'align-self': 'unsafe center',
+    },
+    'meta': {
+      'group': 'alignSelf',
+      'order': 13,
+    },
+  },
+  'self-inherit': {
+    'utility': {
+      'align-self': 'inherit',
+    },
+    'meta': {
+      'group': 'alignSelf',
+      'order': 14,
+    },
+  },
+  'self-initial': {
+    'utility': {
+      'align-self': 'initial',
+    },
+    'meta': {
+      'group': 'alignSelf',
+      'order': 15,
+    },
+  },
+  'self-unset': {
+    'utility': {
+      'align-self': 'unset',
+    },
+    'meta': {
+      'group': 'alignSelf',
+      'order': 16,
+    },
+  },
+
   // end export
 };
