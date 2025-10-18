6fdcddacf09722e8fefce24b982d29bff084f3de
nailuslabs <nailuslab@gmail.com>
2025-10-18 08:21:42 +0200

    mise a jours static des classes align-content

--- src/lib/utilities/static.ts
+++ src/lib/utilities/static.ts
@@ -3,6 +3,7 @@ import type { StaticUtility } from '../../interfaces';
 export const staticUtilities: StaticUtility = {
   // alignContent
   // Utilities for controlling how rows are positioned in multi-row flex and grid containers.
+  // Normal alignment
   'content-normal': {
     'utility': {
       'align-content': 'normal',
@@ -10,6 +11,182 @@ export const staticUtilities: StaticUtility = {
     'meta': {
       'group': 'alignContent',
       'order': 1,
+    },
+  },
+  // Basic positional alignment
+  'content-start': {
+    'utility': {
+      'align-content': 'start',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 2,
+    },
+  },
+  'content-center': {
+    'utility': {
+      'align-content': 'center',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 3,
+    },
+  },
+  'content-end': {
+    'utility': {
+      'align-content': 'end',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 4,
+    },
+  },
+  'content-flex-start': {
+    'utility': {
+      'align-content': 'flex-start',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 5,
+    },
+  },
+  'content-flex-end':   {
+    'utility': {
+      'align-content': 'flex-end',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 6,
+    },
+  },
+  // Baseline alignment
+  'content-baseline':       {
+    'utility': {
+      'align-content': 'baseline',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 7,
+    },
+  },
+  'content-first-baseline': {
+    'utility': {
+      'align-content': 'first baseline',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 8,
+    },
+  },
+  'content-last-baseline':  {
+    'utility': {
+      'align-content': 'last baseline',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 9,
+    },
+  },
+  // Distributed alignment
+  'content-between': {
+    'utility': {
+      'align-content': 'space-between',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 10,
+    },
+  },
+  'content-around':  {
+    'utility': {
+      'align-content': 'space-around',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 11,
+    },
+  },
+  'content-evenly':  {
+    'utility': {
+      'align-content': 'space-evenly',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 12,
+    },
+  },
+  'content-stretch': {
+    'utility': {
+      'align-content': 'space-stretch',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 13,
+    },
+  },
+  // Overflow alignment
+  'content-safe-center':   {
+    'utility': {
+      'align-content': 'safe center',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 14,
+    },
+  },
+  'content-unsafe-center': {
+    'utility': {
+      'align-content': 'unsafe center',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 15,
+    },
+  },
+  // Global values
+  'content-inherit': {
+    'utility': {
+      'align-content': 'inherit',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 16,
+    },
+  },
+  'content-initial': {
+    'utility': {
+      'align-content': 'initial',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 17,
+    },
+  },
+  'content-revert':  {
+    'utility': {
+      'align-content': 'revert',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 18,
+    },
+  },
+  'content-revert-layer': {
+    'utility': {
+      'align-content': 'revert-layer',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 19,
+    },
+  },
+  'content-unset': {
+    'utility': {
+      'align-content': 'unset',
+    },
+    'meta': {
+      'group': 'alignContent',
+      'order': 20,
     },
   },
 };
