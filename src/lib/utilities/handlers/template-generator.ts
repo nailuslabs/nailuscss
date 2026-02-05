26c747788b92a9f56babf4c9b64885abd258e7e7
nailuslab <nailuslab@gmail.com>
2026-02-05 09:22:13 +0100

    init template-generator.ts

--- src/lib/utilities/handlers/template-generator.ts
+++ src/lib/utilities/handlers/template-generator.ts
@@ -1,1 +1,15 @@
+/**
+ * GÉNÉRATEUR UNIVERSEL DE UTILITIES
+ * 
+ * Supporte :
+ * StaticUtility (utilitaires statiques)
+ * DynamicUtility (utilitaires dynamiques avec fonctions)
+ * Regex patterns pour détection automatique
+ * Aliases centralisés (ZERO duplication)
+ * Avec ET sans suffixes
+ * Normalisation automatique
+ * 
+ * À utiliser dans TOUS les modules (svg.ts, background.ts, flex.ts, etc.)
+ */
 
+import type { StaticUtility, DynamicUtility } from '../../../interfaces';
